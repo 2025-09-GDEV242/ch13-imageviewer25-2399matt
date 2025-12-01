412afd9810b5d326ca9d43b1921291e91be27aed
2399matt <notoriousQR@gmail.com>
2025-12-01 11:47:22 -0500

    Warhol, maybe

--- ImageViewer.java
+++ ImageViewer.java
@@ -262,6 +262,7 @@ public class ImageViewer
         filterList.add(new RedTintFilter("Red Tint"));
         filterList.add(new BlueTintFilter("Blue Tint"));
         filterList.add(new GreenTintFilter("Green Tint"));
+        filterList.add(new WarholFilter("Warhol:"));
 
         return filterList;
     }
---
+++ WarholFilter.java
@@ -0,0 +1,44 @@
+
+/**
+ * Warhol filter class for images.
+ *
+ * @author Matt Witham
+ * @version 11.30.25
+ */
+public class WarholFilter extends Filter
+{
+
+    public WarholFilter(String name) {
+        super(name);
+    }
+
+    @Override
+    public void apply(OFImage image) {
+
+        int halfWidth = image.getWidth() / 2;
+        int halfHeight = image.getHeight() / 2;
+
+        OFImage red   = new OFImage(image);
+        OFImage green = new OFImage(image);
+        OFImage blue  = new OFImage(image);
+
+        new RedTintFilter("").apply(red);
+        new GreenTintFilter("").apply(green);
+        new BlueTintFilter("").apply(blue);
+        //brute forcing can be fun.
+        copy(image, image, 0, 0, halfWidth, halfHeight);
+        copy(red, image, halfWidth, 0, halfWidth, halfHeight);
+        copy(green, image, 0, halfHeight, halfWidth, halfHeight);
+        copy(blue, image, halfWidth, halfHeight, halfWidth, halfHeight);
+    }
+
+    private void copy(OFImage og, OFImage newImage, int xOffset, int yOffset, int targetWidth, int targetHeight) {
+        for (int x = 0; x < targetWidth; x++) {
+            for (int y = 0; y < targetHeight; y++) {
+                int srcX = x * og.getWidth() / targetWidth;
+                int srcY = y * og.getHeight() / targetHeight;
+                newImage.setPixel(x + xOffset, y + yOffset, og.getPixel(srcX, srcY));
+            }
+        }
+    }
+}
